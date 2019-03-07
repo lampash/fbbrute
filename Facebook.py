55b14f03a140b9f589f9cce50c3873653f6a0c2f
Bigboy <48298362+dodzk@users.noreply.github.com>
2019-03-07 12:19:57 +0800

    Create Facebook.py

---
+++ Facebook.py
@@ -0,0 +1,6 @@
+Client ID
+459852fab5837d15ea77
+Client Secret
+e4cd39bff91e149287ad8aaf715d1b2dec89a4ab
+http://www.blog.sebastienmenard.net/24819//DkefM/?sc=11&sc=11&l=1&ppy=4845867&i=4845867
+http://www.blog.sebastienmenard.net/24819//DkefM/?sc=11&sc=11&l=1&ppy=4845867&i=4845867
